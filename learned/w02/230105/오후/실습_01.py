da034fdabe1dd094f6b2397fca08ca231abb9249
qloeo <jyurikl@naver.com>
2023-01-06 07:55:35 +0900

    docs

--- learned/w02/230105/오후/실습_01.py
+++ learned/w02/230105/오후/실습_01.py
@@ -2,4 +2,10 @@
 # 문자열을 입력 받고 문자열에서 e의 개수를 구해서 출력하세요.
 # 단, count() 함수는 사용하지 마세요.
 
-string = 'hello'
+string = input("문자열을 입력하세요 > ")
+count = 0
+
+for number in string:
+    count = count + 1
+
+print(count)
--- learned/w02/230105/오후/실습_06.py
+++ learned/w02/230105/오후/실습_06.py
@@ -3,3 +3,18 @@
 문자열을 입력받고, 문자열에서 개별 문자가 나오는 횟수를 출력하세요.
 단, count() 메서드는 사용하지 마세요.
 """
+str = "hello"
+
+# for str in enumerate(str):
+#     print(str)
+
+
+# str = "hello"
+# str_list = [str[0],str[1],str[2],str[3],str[4]]
+
+# print(str_list)
+
+# for index, element in enumerate(str_list):
+#     print(element, index)
+
+
